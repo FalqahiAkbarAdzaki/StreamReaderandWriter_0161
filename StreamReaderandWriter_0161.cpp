3e53a021be1fcc8fbe47545ab652afbce62c5859
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-06-04 10:21:29 +0700

    10

--- StreamReaderandWriter_0161.cpp
+++ StreamReaderandWriter_0161.cpp
@@ -24,5 +24,10 @@ int main() {
 	ifstream infile;
 	infile.open("contohfile.txt");
 
-	cout << endl < ">= Membuka dan membaca file " << endl;
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	if (infile.is_open())
+	{
+
+		while (getline(infile, baris))
+	}
 }
