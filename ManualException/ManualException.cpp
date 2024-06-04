812eab6dc68f2ba8f6ef85edf690226ff619544b
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-06-04 10:57:34 +0700

    25

--- ManualException/ManualException.cpp
+++ ManualException/ManualException.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int mainb
+int main()
+{
+	try {
+		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		cout << "Pertanyaan tidak akan dieksekusi" << endl;
+	}
+	catc
+}
